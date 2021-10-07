dae6b10ba7109a88d95ac6ea7327e05546531f97
tga <tga@space55.xyz>
2021-10-07 13:50:59 -0400

    report err

--- scripts/demo.js
+++ scripts/demo.js
@@ -5,22 +5,26 @@ const port = process.env.PORT || 8000
 console.log(`http://localhost:${port}`)
 
 http.createServer((req, res) => {
-	res.writeHead(200, {
-		"Content-Type": "text/html"
-	});
-	res.write(`
-<html>
-<head>
-	<script>
-${fs.readFileSync("dist/kaboom.js")}
-	</script>
-</head>
-<body>
-	<script>
-${fs.readFileSync(`demo/${req.url}.js`)}
-	</script>
-</body>
-</html>
-	`);
-	res.end();
+	try {
+		res.writeHead(200, {
+			"Content-Type": "text/html"
+		});
+		res.end(`
+	<html>
+	<head>
+		<script>
+			${fs.readFileSync("dist/kaboom.js")}
+		</script>
+	</head>
+	<body>
+		<script>
+			${fs.readFileSync(`demo/${req.url}.js`)}
+		</script>
+	</body>
+	</html>
+		`);
+	} catch (err) {
+		res.writeHead(500);
+		res.end(err.stack);
+	}
 }).listen(port);
